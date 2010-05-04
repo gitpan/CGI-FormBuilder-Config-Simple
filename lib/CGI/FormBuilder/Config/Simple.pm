934ed0b317a2d5d68c5815107a2a7d07ba66595c
Hugh Esco <hesco@campaignfoundations.com>
2010-05-04 16:25:22 +0000

    Import of HESCO/CGI-FormBuilder-Config-Simple-0.12 from CPAN.
    gitpan-cpan-distribution: CGI-FormBuilder-Config-Simple
    gitpan-cpan-version:      0.12
    gitpan-cpan-path:         HESCO/CGI-FormBuilder-Config-Simple-0.12.tar.gz
    gitpan-cpan-author:       HESCO
    gitpan-cpan-maturity:     released

--- lib/CGI/FormBuilder/Config/Simple.pm
+++ lib/CGI/FormBuilder/Config/Simple.pm
@@ -12,11 +12,11 @@ CGI::FormBuilder::Config::Simple - deploy web forms w/ .ini file
 
 =head1 VERSION
 
-Version 0.11
+Version 0.12
 
 =cut
 
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 
 =head1 SYNOPSIS
 
